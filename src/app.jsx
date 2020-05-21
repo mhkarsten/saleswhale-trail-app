be03f74d7676016f0ef28800e6e23c4ba22f4209
Max <m.karsten10@gmail.cim>
2020-05-21 14:38:09 +0200

    Initial empty files for structure

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style/app.scss';
+
+function App() {
+
+
+  return (
+    <div className="app">
+      This is the start of the application!!
+    </div>
+  );
+}
+
+export default App;
